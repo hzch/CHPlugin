6c840a39f2702f99f5c6178ffb88d9b3f8a692fe
hzch <jch.main@gmail.com>
2016-04-08 09:52:28 +0800

    Hello world with Xcode plugin

---
+++ CHPlugin/CHPlugin.h
@@ -0,0 +1,13 @@
+//
+//  CHPlugin.h
+//  CHPlugin
+//
+//  Created by Jiang on 16/4/1.
+//  Copyright © 2016年 hzch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CHPlugin : NSObject
+
+@end
---
+++ CHPlugin/CHPlugin.m
@@ -0,0 +1,17 @@
+//
+//  CHPlugin.m
+//  CHPlugin
+//
+//  Created by Jiang on 16/4/1.
+//  Copyright © 2016年 hzch. All rights reserved.
+//
+
+#import "CHPlugin.h"
+
+@implementation CHPlugin
+
++(void)pluginDidLoad:(NSBundle *)plugin {
+    NSLog(@"Hello World");
+}
+
+@end
