e6e56a3c6c82b7315c0c4911eaddfff2fae22f32
hzch <jch.main@gmail.com>
2016-04-08 11:01:27 +0800

    all notification

--- CHPlugin/CHPlugin.m
+++ CHPlugin/CHPlugin.m
@@ -10,8 +10,38 @@
 
 @implementation CHPlugin
 
+/// 系统接口，Xcode启动时会调
 +(void)pluginDidLoad:(NSBundle *)plugin {
+    [self sharedInstance];
     NSLog(@"Hello World");
+}
+
+/// 保证生命周期与Xcode相同
++ (instancetype)sharedInstance {
+    static id instance = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        instance = [[self alloc] init];
+    });
+    return instance;
+}
+
+- (instancetype)init {
+    if (self = [super init]) {
+        [self allNotification];
+    }
+    return self;
+}
+
+- (void)allNotification {
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(notificationListener:)
+                                                 name:nil
+                                               object:nil];
+}
+
+-(void)notificationListener:(NSNotification *)noti {
+    NSLog(@" Notification: %@", [noti name]);   
 }
 
 @end
