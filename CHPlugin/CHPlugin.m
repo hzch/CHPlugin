2e22a67ec429972348a288ceedbc2606c093d87d
hzch <jch.main@gmail.com>
2016-04-09 16:29:57 +0800

    goto documents version 1.0

--- CHPlugin/CHPlugin.m
+++ CHPlugin/CHPlugin.m
@@ -8,10 +8,27 @@
 
 #import "CHPlugin.h"
 
+@interface NSObject (IgnoreUndefinedKey)
+
+@end
+
+@implementation NSObject (IgnoreUndefinedKey)
+
+- (nullable id)valueForUndefinedKey:(NSString *)key
+{
+    return nil;
+}
+
+@end
+
 static NSString * const kCHPluginsMenuTitle = @"Plugins";
 
 @interface CHPlugin ()
 @property (nonatomic) NSMenuItem *pluginsMenuItem;
+@property (nonatomic) NSString *currentBundleId;
+@property (nonatomic) NSString *currentDeviceAppPath;
+@property (nonatomic) NSString *currentDocuments;
+@property (nonatomic) NSFileManager *fileManager;
 
 @end
 
@@ -41,6 +58,11 @@ static NSString * const kCHPluginsMenuTitle = @"Plugins";
                                                  selector:@selector(didApplicationFinishLaunchingNotification:)
                                                      name:NSApplicationDidFinishLaunchingNotification
                                                    object:nil];
+        
+        [[NSNotificationCenter defaultCenter] addObserver:self
+                                                 selector:@selector(buildWillStart:)
+                                                     name:@"IDEBuildOperationWillStartNotification"
+                                                   object:nil];
     }
     return self;
 }
@@ -54,7 +76,7 @@ static NSString * const kCHPluginsMenuTitle = @"Plugins";
 - (void)addPluginMenu
 {
     NSMenuItem* menuItem = [NSMenuItem new];
-    menuItem.title = @"test";
+    menuItem.title = @"Go To Documents";
     menuItem.target = self;
     menuItem.action = @selector(test);
     [self.pluginsMenuItem.submenu addItem:menuItem];
@@ -62,8 +84,148 @@ static NSString * const kCHPluginsMenuTitle = @"Plugins";
 
 - (void)test
 {
-    NSLog(@"test");
+    NSString *currentDocuments = self.currentDocuments;
+    if (currentDocuments.length == 0) {
+        NSAlert *alert = [[NSAlert alloc] init];
+        [alert addButtonWithTitle:@"OK"];
+        [alert setMessageText:@"Not found documents!"];
+        [alert setInformativeText:@"Please build or run with your project. If it still doesn't work, send email to jch.main@gmail.com."];
+        [alert setAlertStyle:NSWarningAlertStyle];
+        [alert runModal];
+        return;
+    }
     
+    NSString *open = [NSString stringWithFormat:@"open %@",self.currentDocuments];
+    const char *str = [open UTF8String];
+    system(str);
+}
+
+#pragma mark - install BundleId and Simulator
+- (void)buildWillStart:(NSNotification *)notification
+{
+    NSLog(@"[CHPlugin] build will start.");
+    self.currentDocuments = nil;
+    self.currentBundleId = [self.class bundleIdInNotification:notification];
+    self.currentDeviceAppPath = [self.class deviceAppPathInNotification:notification];
+}
+
+- (NSString *)currentDocuments
+{
+    if (_currentDocuments) {
+        return _currentDocuments;
+    }
+    
+    if (self.currentDeviceAppPath.length == 0 || self.currentBundleId.length == 0) {
+        return nil;
+    }
+    
+    NSArray *paths = [self.fileManager contentsOfDirectoryAtPath:self.currentDeviceAppPath error:nil];
+    for (NSString *pathName in paths) {
+        NSString *fileName = [self.currentDeviceAppPath stringByAppendingPathComponent:pathName];
+        NSString *fileUrl = [fileName stringByAppendingPathComponent:@".com.apple.mobile_container_manager.metadata.plist"];
+        
+        if([self.fileManager fileExistsAtPath:fileUrl]){
+            NSDictionary *dict = [NSDictionary dictionaryWithContentsOfFile:fileUrl];
+            NSString *bundleId = [dict valueForKeyPath:@"MCMMetadataIdentifier"];
+            if (bundleId.length != 0 && [self.currentBundleId isEqualToString:bundleId]) {
+                _currentDocuments = [fileName stringByAppendingPathComponent:@"Documents"];
+                return _currentDocuments;
+            }
+        }
+    }
+    
+    return nil;
+}
+
+
++ (NSString*)deviceAppPathInNotification:(NSNotification *)notification
+{
+    NSString *deviceId = [self.class deviceIdInNotification:notification];
+    if (deviceId.length == 0) {
+        return nil;
+    }
+    return [NSString stringWithFormat:@"%@/Library/Developer/CoreSimulator/Devices/%@/data/Containers/Data/Application", NSHomeDirectory(),deviceId];
+    
+}
+
++ (NSString*)deviceIdInNotification:(NSNotification *)notification
+{
+    id IDEBuildParameters = [notification.object valueForKey:@"_buildParameters"];
+    if (![IDEBuildParameters isKindOfClass:NSClassFromString(@"IDEBuildParameters").class]) {
+        NSLog(@"No _buildParameters in noti.object.");
+        return nil;
+    }
+    
+    id IDERunDestination = [IDEBuildParameters valueForKey:@"_activeRunDestination"];
+    if (![IDERunDestination isKindOfClass:NSClassFromString(@"IDERunDestination").class]) {
+        NSLog(@"No _activeRunDestination in IDEBuildParameters.");
+        return nil;
+    }
+    
+    id DVTiPhoneSimulator = [IDERunDestination valueForKey:@"_targetDevice"];
+    if (![DVTiPhoneSimulator isKindOfClass:NSClassFromString(@"DVTiPhoneSimulator").class]) {
+        NSLog(@"No _targetDevice in IDERunDestination.");
+        return nil;
+    }
+    
+    NSString *identifier = [DVTiPhoneSimulator valueForKey:@"_identifier"];
+    if (![identifier isKindOfClass:NSString.class]) {
+        NSLog(@"No _identifier in DVTiPhoneSimulator.");
+        return nil;
+    }
+
+    return identifier;
+}
+
++ (NSString*)bundleIdInNotification:(NSNotification *)notification
+{
+    NSString *infoPath = [self infoPathInNotification:notification];
+    if (infoPath.length == 0) {
+        return nil;
+    }
+    NSDictionary *dic = [NSDictionary dictionaryWithContentsOfFile:infoPath];
+    return dic[@"CFBundleIdentifier"];
+}
+
++ (NSString*)infoPathInNotification:(NSNotification *)notification
+{
+    NSArray *buildables = [notification.object valueForKey:@"_buildables"];
+    if (![buildables isKindOfClass:NSArray.class]) {
+        NSLog(@"No _buildables in noti.object.");
+        return nil;
+    }
+    
+    id Xcode3TargetProduct = buildables[0];
+    if (![Xcode3TargetProduct isKindOfClass:NSClassFromString(@"Xcode3TargetProduct").class]) {
+        NSLog(@"No Xcode3TargetProduct in buildables.");
+        return nil;
+    }
+    
+    id Xcode3Target = [Xcode3TargetProduct valueForKey:@"_blueprint"];
+    if (![Xcode3Target isKindOfClass:NSClassFromString(@"Xcode3Target").class]) {
+        NSLog(@"No _blueprint in Xcode3TargetProduct.");
+        return nil;
+    }
+    
+    id PBXNativeTarget = [Xcode3Target valueForKey:@"_pbxTarget"];
+    if (![PBXNativeTarget isKindOfClass:NSClassFromString(@"PBXNativeTarget").class]) {
+        NSLog(@"No _pbxTarget in Xcode3Target.");
+        return nil;
+    }
+    
+    id PBXFileReference = [PBXNativeTarget valueForKey:@"_infoPlistRef"];
+    if (![PBXFileReference isKindOfClass:NSClassFromString(@"PBXFileReference").class]) {
+        NSLog(@"No _infoPlistRef in PBXNativeTarget.");
+        return nil;
+    }
+    
+    NSString *infoPath = [PBXFileReference valueForKey:@"_absolutePath"];
+    if (![infoPath isKindOfClass:NSString.class]) {
+        NSLog(@"No _absolutePath in PBXNativeTarget.");
+        return nil;
+    }
+    
+    return infoPath;
 }
 
 #pragma mark - all notification
@@ -77,7 +239,7 @@ static NSString * const kCHPluginsMenuTitle = @"Plugins";
 
 -(void)notificationListener:(NSNotification *)noti
 {
-    NSLog(@" Notification: %@", [noti name]);   
+    NSLog(@"Notification: %@", [noti name]);   
 }
 
 #pragma mark - setter & getter
@@ -96,6 +258,14 @@ static NSString * const kCHPluginsMenuTitle = @"Plugins";
         [mainMenu addItem:_pluginsMenuItem];
     }
     return _pluginsMenuItem;
+}
+
+- (NSFileManager *)fileManager
+{
+    if (!_fileManager) {
+        _fileManager = [NSFileManager defaultManager];
+    }
+    return _fileManager;
 }
 
 @end
